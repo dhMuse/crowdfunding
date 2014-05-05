404a85d872cb467d60cc9b2a7dfe04b56be9af84
dhMuse <danielmusekamp@icloud.com>
2014-05-05 16:05:33 -0700

    fixed tests

--- spec/controllers/api/v1/campaigns_controller_spec.rb
+++ spec/controllers/api/v1/campaigns_controller_spec.rb
@@ -5,12 +5,12 @@ describe Api::V1::CampaignsController do
 
   context "with valid api key" do
     let(:user) { create(:user) }
-    
+
     describe "fetching all campaigns" do
       let!(:campaign_1) { create(:campaign)}
       let!(:campaign_2) { create(:campaign)}
       before do
-        get :index, format: :json
+        get :index, format: :json, access_token: user.api_key.access_token
       end
       specify { response.body.should include(campaign_1.title) }
       specify { response.body.should include(campaign_1.details) }
@@ -21,7 +21,7 @@ describe Api::V1::CampaignsController do
     describe "fetching a single campaign" do
       let(:campaign) { create(:campaign) }
       before do
-        get :show, id: campaign.id, format: :json
+        get :show, id: campaign.id, format: :json, access_token: user.api_key.access_token
       end
 
       it "includes campaign's title" do
